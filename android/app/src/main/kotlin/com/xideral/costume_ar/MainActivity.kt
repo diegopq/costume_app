a4549d4846250cf6f838caedc60ed8df5a64f243
diegopq <d.padillaquijas@gmail.com>
2023-10-22 20:31:55 -0600

    initial config

---
+++ android/app/src/main/kotlin/com/xideral/costume_ar/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.xideral.costume_ar
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/core/utils/env.dart
@@ -0,0 +1,6 @@
+part of 'utils.dart';
+
+class Env {
+  static const deppArAndroidKey = String.fromEnvironment('DEPP_AR_ANDROID');
+  static const deppArIosKey = String.fromEnvironment('DEPP_AR_IOS');
+}
